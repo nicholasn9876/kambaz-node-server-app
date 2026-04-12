642d2469ecdb710150e5d28d97e60e4a60e74423
Nicholas Nguyen <255169637+nicholasn9876@users.noreply.github.com>
2026-04-11 22:45:53 -0400

    server with db

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import cors from 'cors';
 import "dotenv/config";
 import session from "express-session";
@@ -11,6 +12,8 @@ import ModulesRoutes from './kambaz/modules/routes.js';
 import AssignmentsRoutes from './kambaz/assignments/routes.js';
 import EnrollmentsRoutes from './kambaz/enrollments/routes.js';
 
+const CONNECTION_STRING = process.env.DATABASE_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kambaz";
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(cors({
   credentials: true,
